49c870273f84b288a23c7509d0ea014369359ad0
Marcos Túlio <tuliocct@hotmail.com>
2013-04-12 16:36:13 -0300

    Atualizado 12/04 - 15:00

--- SICHROLAB/build/generated-sources/ap-source-output/modelo/CategoriaDeEvento_.java
+++ SICHROLAB/build/generated-sources/ap-source-output/modelo/CategoriaDeEvento_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-04-11T22:16:46")
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-04-12T15:01:58")
 @StaticMetamodel(CategoriaDeEvento.class)
 public class CategoriaDeEvento_ { 
 
