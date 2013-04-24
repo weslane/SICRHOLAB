c9ed05f6ab7135da8ca57d47d98b95054ece9716
Weslane <weslane.almeida@hotmail.com>
2013-04-24 19:50:13 -0300

    implementação do login sem autenticação de páginas e correção na entidade Usuario

--- SICHROLAB/build/generated-sources/ap-source-output/modelo/Usuario_.java
+++ SICHROLAB/build/generated-sources/ap-source-output/modelo/Usuario_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import modelo.CategoriaUsuario;
 
-@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-04-24T18:08:12")
+@Generated(value="EclipseLink-2.3.2.v20111125-r10461", date="2013-04-24T18:32:06")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
